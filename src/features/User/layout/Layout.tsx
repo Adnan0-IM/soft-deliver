bcd485276474b44c4a332202da162a3c8f372e43
Adnan0-IM <adnanilyas969@gmail.com>
2025-12-11 14:25:28 +0100

    feat: add Sidebar and Topbar components for user layout with navigation links

--- src/features/User/layout/Layout.tsx
+++ src/features/User/layout/Layout.tsx
@@ -1,9 +1,19 @@
 import { Outlet } from "react-router";
+import Sidebar from "./Sidebar";
+import Topbar from "./Topbar";
 
 export default function UserLayout() {
   return (
-    <div>
-      <Outlet />
+    <div className="flex h-screen  overflow-hidden">
+      <Sidebar />
+
+      <div className="flex flex-col md:pl-64 flex-1">
+        <Topbar />
+
+        <main className="p-6 overflow-y-auto flex-1">
+          <Outlet />
+        </main>
+      </div>
     </div>
   );
 }
--- src/features/User/layout/Topbar.tsx
+++ src/features/User/layout/Topbar.tsx
@@ -3,9 +3,9 @@ import { Bell } from "lucide-react";
 
 export default function Topbar() {
   return (
-    <header className="h-16 bg-card border-b border-border flex items-center justify-between px-4 md:px-6">
+  <header className="h-16 bg-card border-b border-border flex items-center justify-between px-4 md:px-6">
       <div className="flex items-center gap-3">
-        <h1 className="text-lg font-semibold">Admin</h1>
+        <h1 className="text-lg font-semibold">Welcome</h1>
       </div>
 
       <div className="flex items-center gap-4">
